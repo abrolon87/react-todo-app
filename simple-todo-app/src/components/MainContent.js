556f09e662dffbd1931f400c602d2077b016cc16
Amanda Rolon <abrolon87@gmail.com>
2020-08-23 02:36:57 -0400

    adds TodoItem components

---
+++ simple-todo-app/src/components/MainContent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import TodoItem from './TodoItem'
+
+function MainContent() {
+  return (
+    <div className="todo-list">
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+    </div>
+  )
+}
+
+export default MainContent
