3ad3f3e5735f5c54aaab73beb6ba0bec752d6ef9
Amanda Rolon <abrolon87@gmail.com>
2020-08-24 13:21:43 -0400

    adds fake data

---
+++ simple-todo-app/src/todosData.js
@@ -0,0 +1,24 @@
+const todosData = [
+  {
+    id: 1,
+    text: "Walk the dog",
+    completed: true
+  },
+  {
+    id: 2,
+    text: "Vacuum the living room",
+    completed: false
+  },
+  {
+    id: 3,
+    text: "Clean the bathroom",
+    completed: true
+  },
+  {
+    id: 4,
+    text: "Wash the dishes",
+    completed: false 
+  }
+]
+
+export default todosData
