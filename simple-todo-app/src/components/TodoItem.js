7736e3cef74d8e84e7b49f1c7888591987a003e4
Amanda Rolon <abrolon87@gmail.com>
2020-08-24 23:45:04 -0400

    adds onChange event

--- simple-todo-app/src/components/TodoItem.js
+++ simple-todo-app/src/components/TodoItem.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import todosData from '../todosData'
 
-function TodoItem() {
-  
+function TodoItem(props) {
+    
     return (
       <div className="todo-item">
-        <input type="checkbox" />
-        <p>Placeholder text here</p> 
+        <input type="checkbox" checked={props.item.completed} onChange={() => props.handleChange(props.item.id)}/>
+        <p>{props.item.text}</p> 
       </div>
     )
   
